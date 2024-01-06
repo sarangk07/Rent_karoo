b46118457a8c1ce5f2084cd30fe14b7026765a25
Sarang <sarang00005@gmail.com>
2024-01-06 11:19:59 +0530

    final3

--- rentCars/settings.py
+++ rentCars/settings.py
@@ -145,12 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "templates/static/"
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'templates\static'),
 # ]
-STACTIC_ROOT = "static/"
+STACTIC_ROOT = "templates/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/static")]
 MEDIA_URL = "media/"
