1641bdabf39c8ac5c0d85d8b030e74fe5d1e6b32
Sarang <sarang00005@gmail.com>
2024-01-09 09:03:26 +0530

    final9

--- rentCars/settings.py
+++ rentCars/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'NAME': 'rentkaroDB',
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST"),
+        'HOST': 'rentkaro.cpaw0aow0cjk.eu-north-1.rds.amazonaws.com',
         'PORT': '5432'
     }
     
@@ -204,3 +204,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+
